120295d5ada21e535e0a32f24559886655b21b7b
dpay <deviap485@gmail.com>
2020-02-27 10:55:11 +0700

    add numerik without air resistance D

--- tubes.py
+++ tubes.py
@@ -1,6 +1,6 @@
 import math
 import matplotlib.pyplot as plt
-
+import numpy as np
 #Gerak Peluru Tanpa memikirkan Hambatan Udata(D)
 
 g=9.806
@@ -34,8 +34,10 @@ numerik2x=[]
 numerik2y=[]
 analitik1x=[]
 analitik1y=[]
+analitik2x=[]
+analitik2y=[]
 #NUMERIK TANPA HAMBATAN UDARA
-i=0.01
+i=dt
 x=0
 y=0
 numerik1x.append(x)
@@ -49,57 +51,82 @@ while(i<=T):
     y=y+(vyt*dt)
     numerik1x.append(round(x,2))
     numerik1y.append(round(y,2))
-    i=i+0.01
-print(i)
-#ANALITIK PEMBVADING
-t=0.01
+    i=i+dt
+print('total waktu numerik tanpa hambatan',i)
+#ANALITIK PEMBVADING TANPA HAMBATAN UDARA
+t=dt
 x0=0
 y0=0
 analitik1x.append(x0)
 analitik1y.append(y0)
 
 while(t<=T):
-    x=round(vx,2)*t
-    y=(round(vy,2)*t)+(0.5*ay*(t**2))
+    x=x0+(round(vx,2)*t)+(0.5*ax*(t**2)) #bisa rubah jadi x0*round(vx,2)*t
+    y=y0+(round(vy,2)*t)+(0.5*ay*(t**2))
     
     analitik1x.append(round(x,2))
     analitik1y.append(round(y,2))
-    t=t+0.01
+    t=t+dt
+print('total waktu analitik tanpa hambatan',t)
 #NUMERIK DENGAN HAMBATAN UDARA
 g=9.806
 
 
-i=0.01
+i=dt
 x=0
 y=0
 
 numerik2x.append(x)
 numerik2y.append(y)
-vxt=vx
-vyt=vy
+v = math.sqrt((np.power(vx,2)+np.power(vy,2)))
+ax2 = -(D/m)*v*vx
+ay2 = -g-(D/m)*v*vy
 while (i<=T) :
-    v = math.sqrt((vx**2+vy**2))
-    ax2 = -(D/m)*v*vx
-    ay2 = -g-(D/m)*v*vy
+
     vy = vy + (ay2 * dt)
     vx = vx + (ax2 * dt)
     y = y + (vy * dt)
     x = x + (vx * dt)
     numerik2x.append(x)
     numerik2y.append(y)
+    if(y<0):        #kenapa gak pake waktu jadi if(i>=5.89):
+        break       #                                break
+    else:           #Karena nanti pada detik ke 4.50 keatas y nya sudah minus sehingga 
+        i=i+dt      #grafik tidak berakhir di y=0 jadi anggep ajah si peluru berhenti ditanah itu pada (y=0)
+print('total waktu numerik hambatan',i)
+
+#ANALITIK PEMBVADING DENGAN HAMBATAN UDARA
+vy = v0*math.sin(theta) #vy(1)
+vx = v0*math.cos(theta) #konstant
+t=dt
+x0=0
+y0=0
+analitik2x.append(x0)
+analitik2y.append(y0)
+
+while(t<=T):
+    v = math.sqrt((np.power(vx,2)+np.power(vy,2)))
+    ax = -(D/m)*v*vx
+    ay = -g-(D/m)*v*vy
+    x=x0+(round(vx,2)*t)+(0.5*ax*(t**2)) #bisa rubah jadi x0*round(vx,2)*t
+    y=y0+(round(vy,2)*t)+(0.5*ay*(t**2))
+    
+    analitik2x.append(round(x,2))
+    analitik2y.append(round(y,2))
     if(y<0):
         break
     else:
-        i=i+0.01
-print(i)
-
+        t=t+dt
+print('total waktu analitik hambatan',t)
 
 #print(numerik1x)
 #print(numerik1y)
+#print(analitik1x)
+#print(analitik1y)
 #print(numerik2x)
 #print(numerik2y)
 plt.plot(numerik1x,numerik1y,'r')
 plt.plot(numerik2x,numerik2y,'b')
-plt.plot(analitik1x,analitik1y,'y')
-#plt.plot(KDataNumerikx,KDataNumeriky,'b')
+plt.plot(analitik1x,analitik1y,'g')
+plt.plot(analitik2x,analitik2y,'black')
 plt.show()
